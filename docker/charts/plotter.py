701bc2ed0889cbbf833c8615ebc7378953abab55
dom <davide.domini@unibo.it>
2024-03-07 18:16:55 +0100

    feat: now plotter is parametric and plots for both mnist and fashion-mnist experiments

--- docker/charts/plotter.py
+++ docker/charts/plotter.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import glob
+import sys
 
 dataframes = []
+experiment = sys.argv[1]
 
-fbfl_df = pd.read_csv('results_fbfl_mnist.csv', sep=' ')
+fbfl_df = pd.read_csv(f'data/results_fbfl_{experiment}.csv', sep=' ')
 fbfl_df['Learning type'] = 'Field-based FL'
 fbfl_df = fbfl_df[['time', 'Accuracy[mean]', 'Loss[mean]', 'TrainLoss[mean]', 'Learning type']]
 fbfl_df = fbfl_df.rename(columns={'time':'Epoch', 'Accuracy[mean]': 'Average validation accuracy', 'Loss[mean]': 'Average validation loss', 'TrainLoss[mean]': 'Average train loss'})
 dataframes.append(fbfl_df.iloc[:11])
 
-
-fl_df = pd.read_csv('results_federated.csv')
+fl_df = pd.read_csv(f'data/results_cfl_{experiment}.csv')
 fl_df['Learning type'] = 'Federated Learning'
 fl_df['Epoch'] = [i for i in range(15)]
 dataframes.append(fl_df.iloc[:11])
@@ -27,7 +28,7 @@ ax = sns.lineplot(
 ax.set_xlabel('Epoch', fontsize=18)
 ax.set_ylabel('Average train loss', fontsize=18)
 
-plt.savefig('avg_train_loss_mnist2.png', dpi=500)
+plt.savefig(f'charts/avg_train_loss_{experiment}.png', dpi=500)
 plt.close()
 
 ax = sns.lineplot(
@@ -36,7 +37,7 @@ ax = sns.lineplot(
 )
 ax.set_xlabel('Epoch', fontsize=18)
 ax.set_ylabel('Average validation loss', fontsize=18)
-plt.savefig('avg_validation_loss_mnist2.png', dpi=500)
+plt.savefig(f'charts/avg_validation_loss_{experiment}.png', dpi=500)
 plt.close()
 
 ax = sns.lineplot(
@@ -45,21 +46,6 @@ ax = sns.lineplot(
 )
 ax.set_xlabel('Epoch', fontsize=18)
 ax.set_ylabel('Average validation accuracy', fontsize=18)
-plt.savefig('avg_validation_accuracy_mnist2.png', dpi=500)
+plt.savefig(f'charts/avg_validation_accuracy_{experiment}.png', dpi=500)
 plt.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
